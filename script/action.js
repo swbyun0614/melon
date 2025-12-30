48857f0675a822f0001e49aaa33e9c1e2a8594c9
swbyun0614 <swbyun0614@gmail.com>
2025-12-30 10:40:11 +0900

    햄버거버튼 수정

--- script/action.js
+++ script/action.js
@@ -1,4 +1,11 @@
 $('.hamburger').click(function(){
     $('header nav').fadeToggle().css({display:'flex'});
     $('.hamburger').toggleClass('on');
+});
+
+$(window).resize(function(){
+    if($(window).width() > 768){
+        $('header nav').removeAttr('style');
+        $('.hamburger').removeClass('on');
+    }
 })
